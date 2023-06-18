4590a0204fb01724e2a2a0fcc486d9954547bab1
LeonFernandes <leon.fernandes@columbia.edu>
2023-06-18 00:01:35 -0400

    export simts

--- R/simts.R
+++ R/simts.R
@@ -3,6 +3,7 @@
 #' @param object model from which to simulate. Currently supports objects of
 #'      class `arima` or fitted using `stats::arima` and `modeltime::arima_reg`.
 #' @param ... not used.
+#' @export
 simts <-
     function(object, ...) {
         UseMethod("simts")
