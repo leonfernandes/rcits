31118bf851f3acc8c8dbdd8445ddad1a5d9d01bf
LeonFernandes <leon.fernandes@columbia.edu>
2023-06-18 00:04:24 -0400

    make_arima

--- R/make_arima.R
+++ R/make_arima.R
@@ -4,7 +4,7 @@
 #' @param delta vector of differencing coefficients, so an ARMA model is fitted
 #'      to `y[t] - delta[1]*y[t-1] - ...`.
 #' @export
-arima <-
+make_arima <-
     function(phi, theta, delta) {
         ret <- stats::makeARIMA(phi = phi, theta = theta, Delta = delta)
         class(ret) <- "arima"
