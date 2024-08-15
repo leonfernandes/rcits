6dcde15a5abe7ada1edee82cbce4c8bce68ffed1
LeonFernandes <leon.fernandes@columbia.edu>
2024-08-14 22:52:26 -0400

    untrack .github and remove default S3 methods

--- R/simults.R
+++ R/simults.R
@@ -6,15 +6,6 @@
 #' @export
 simults <- function(object, ...) UseMethod("simults")
 
-#' @rdname simults
-#' @export
-simults.default <-
-    function(object, ...) {
-        rlang::abort(
-            glue::glue("Object of class {class(object)} is not registered.")
-        )
-    }
-
 #' Fitted residuals from registered models
 #'
 #' @param object a registered model. Currently supports objects of class `arima`
@@ -22,11 +13,3 @@ simults.default <-
 #' @param ... not used
 #' @export
 fitted_resid <- function(object, ...) UseMethod("fitted_resid")
-
-#' @rdname fitted_resid
-#' @export
-fitted_resid.default <- function(object, ...) {
-    rlang::abort(
-            glue::glue("Object of class {class(object)} is not registered.")
-        )
-}
